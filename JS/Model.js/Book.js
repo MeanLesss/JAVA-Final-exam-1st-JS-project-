2683403459bd0be3e60a5b2c33a0e772819748d7
MeanLesss <68323176+MeanLesss@users.noreply.github.com>
2022-09-24 17:26:52 +0700

    book interface done now working on sorting and search

--- JS/Model.js/Book.js
+++ JS/Model.js/Book.js
@@ -1,4 +1,5 @@
-export class Book {
+//export class Book {
+class Book {
     constructor(ID, title, authorName, publishingDate, publisher, noPages, copiesInLib) {
         this.ID = ID;
         this.title = title;
@@ -8,4 +9,25 @@ export class Book {
         this.noPages = noPages;
         this.copiesInLib = copiesInLib;
     }
-}
+} // references
+// an object
+// const John = {
+//     name: "John Doe",
+//     age: 23,
+// };
+
+// // convert object to JSON string
+// // using JSON.stringify()
+// const jsonObj = JSON.stringify(John);
+
+// // save to localStorage
+// localStorage.setItem("John", jsonObj);
+
+// // get the string
+// // from localStorage
+// const str = localStorage.getItem("John");
+
+// // convert string to valid object
+// const parsedObj = JSON.parse(str);
+
+// console.log(parsedObj);
--- JS/bookControl.js
+++ JS/bookControl.js
@@ -1,21 +1,127 @@
+// let stock = [
+//     // new Book(101, 'Book hello', 'Stan Lee', '23/10/2002', 'Marvel', 100, 80)
+// ]
+
+let stock = [];
+let jStock = localStorage.getItem("stock");
+stock = JSON.parse(jStock);
+
+function addBook(e) {
+    //e.preventDefault();
+    let book = new Book();
+    if (add.innerHTML === 'Add') {
+        book = new Book(
+            ID.value,
+            title.value,
+            authorName.value,
+            pubDate.value,
+            publisher.value,
+            noPage.value,
+            copies.value);
+        stock.push(book);
+    } else {
+        book = stock.find(b => b.ID == ID.value)
+        console.log(book);
+        book.ID = ID.value;
+        book.title = title.value;
+        book.authorName = authorName.value;
+        book.publishingDate = pubDate.value;
+        book.publisher = publisher.value;
+        book.noPages = noPage.value;
+        book.copiesInLib = copies.value;
+        stock.splice(stock.indexOf(book), 1);
+        stock.push(book)
+    }
+    localStorage.setItem('stock', JSON.stringify(stock));
+    stock = JSON.parse(localStorage.getItem("stock"));
+    console.log(stock)
+    loadTable(stock);
+    closeForm();
+}
+
+function clearStorage(e) {
+    localStorage.clear();
+    stock = [];
+    e.preventDefault();
+    loadTable();
+}
+
 function addForm(e) {
     clearBookInput();
     myForm.style.display = "block";
     formTitle.innerHTML = "Add Book"
-
+    add.innerHTML = "Add";
+    ID.disabled = false;
     e.preventDefault();
 }
 
-function editForm(e) {
+function loadTable(stock) {
+
+    ttbody.innerHTML = '';
+    if (stock != null) {
+        for (let i = 0; i < stock.length; i++) {
+            let tr = document.createElement('tr');
+            for (let j = 0; j < 8; j++) {
+                let th = document.createElement('th');
+                switch (j) {
+                    case 0:
+                        th.innerHTML = stock[i].ID;
+                        break;
+                    case 1:
+                        th.innerHTML = stock[i].title;
+                        break;
+                    case 2:
+                        th.innerHTML = stock[i].authorName;
+                        break;
+                    case 3:
+                        th.innerHTML = stock[i].publishingDate;
+                        break;
+                    case 4:
+                        th.innerHTML = stock[i].publisher;
+                        break;
+                    case 5:
+                        th.innerHTML = stock[i].noPages;
+                        break;
+                    case 6:
+                        th.innerHTML = stock[i].copiesInLib;
+                        break;
+                    case 7:
+                        // button
+                        let button = document.createElement('button');
+                        button.innerHTML = '<u>Edit</u>'
+                        button.addEventListener('click', () => {
+                            editForm(MouseEvent, stock[i]);
+                        })
+                        th.appendChild(button);
+                        break;
+
+                }
+                tr.appendChild(th);
+            }
+            ttbody.appendChild(tr);
+        }
+    }
+
+}
+
+function editForm(e, book) {
     myForm.style.display = "block";
-    formTitle.innerHTML = "Edit Book"
-        // need to add the info into the inputs here
-
-
-    e.preventDefault();
+    formTitle.innerHTML = "Edit Book";
+    add.innerHTML = "Edit"
+    ID.disabled = true;
+    // need to add the info into the inputs here
+    ID.value = book.ID;
+    title.value = book.title;
+    authorName.value = book.authorName;
+    pubDate.value = book.publishingDate;
+    publisher.value = book.publisher;
+    noPage.value = book.noPages;
+    copies.value = book.copiesInLib;
+    //e.preventDefault();
 }
 
 function clearBookInput() {
+    ID.value = "";
     title.value = "";
     authorName.value = "";
     pubDate.value = "";
@@ -27,5 +133,26 @@ function clearBookInput() {
 function closeForm(e) {
     clearBookInput();
     myForm.style.display = "none";
-    e.preventDefault();
+    //e.preventDefault();
 }
+
+function sortBy(sBy) {
+    let sortedArr = [];
+    switch (sBy.toLowerCase()) {
+        case 'id':
+            stock.sort(([, a], [, b]) => {
+                return a[1] - b[1];
+            })
+            break;
+        case 'title':
+            break;
+        case 'authorname':
+            break;
+    }
+    loadTable(stock);
+    console.log(stock);
+}
+
+
+console.log(stock)
+loadTable(stock);
